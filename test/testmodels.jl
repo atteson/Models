7eb6066694427ca0141662dafc9dbb8c85573cf6
Kevin Atteson <kevin@atteson.com>
2019-05-01 18:56:53 -0400

    name change

---
+++ test/testmodels.jl
@@ -0,0 +1,7 @@
+using Models
+using HMMs
+using Brobdingnag
+
+modeldir = joinpath( dirname(dirname(pathof(Models))), "data" )
+
+models = Models.AdaptedModel{HMMs.HMM{HMMs.GenTDist,Brob,Float64}( modeldir )
