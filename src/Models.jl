3e568b7429ce846de2bd68b2cb87ddc197bca42e
Kevin Atteson <kevin@atteson.com>
2019-05-02 10:01:51 -0400

    changes

--- src/Models.jl
+++ src/Models.jl
@@ -7,13 +7,16 @@ end
 
 initialize( M::AbstractModel ) = error( "initialize not yet implemented for $(typeof(M))" )
 
-roll( M::AbstractModel, y ) = error( "roll not yet implemented for $(typeof(M))" )
+roll( M::AbstractModel ) = error( "roll not yet implemented for $(typeof(M))" )
 
-update( M::AbstractModel, y ) = error( "update not yet implemented for $(typeof(M))" )
+update( M::AbstractModel, y::Float64 ) = error( "update not yet implemented for $(typeof(M))" )
+
+rand( M::AbstractModel, n::Int ) = error( "rand not yet implemented for $(typeof(M))" )
 
 mutable struct AdaptedModel{T <: AbstractModel}
     dates::Vector{Date}
     models::Vector{T}
+    index::Int
 end
 
 function AdaptedModel{T}( modeldir::String ) where {T}
@@ -28,7 +31,7 @@ function AdaptedModel{T}( modeldir::String ) where {T}
         end
         push!( dates, Date( m.captures[1], dateformat ) )
 
-        open( file, "r") do f
+        open( joinpath( modeldir, file ), "r") do f
             push!( models, read( f, T ) )
         end
         for model in models
@@ -36,7 +39,21 @@ function AdaptedModel{T}( modeldir::String ) where {T}
         end
     end
 
-    return Model( dates, models )
+    return AdaptedModel( dates, models, 0 )
 end    
+
+function update( M::AdaptedModel{T}, d::Date, y::Float64 ) where {T}
+    if M.index == 0
+        M.index = searchsorted( M.dates, d ).stop
+        if M.index == 0
+            error( "No model available at date $d" )
+        end
+    else
+        while M.index < length(M.dates) && M.dates[M.index+1] < d
+            M.index += 1
+        end
+        update( M.models[M.index], y )
+    end
+end
 
 end # module
