7af4e584a36e2be6bdc3b7a0e7338f8e1f7e1d49
Kevin Atteson <kevin@atteson.com>
2019-05-03 13:14:05 -0400

    changes to rand

--- src/Models.jl
+++ src/Models.jl
@@ -1,6 +1,7 @@
 module Models
 
 using Dates
+using Distributions
 
 abstract type AbstractModel
 end
@@ -11,7 +12,14 @@ roll( M::AbstractModel ) = error( "roll not yet implemented for $(typeof(M))" )
 
 update( M::AbstractModel, y::Float64 ) = error( "update not yet implemented for $(typeof(M))" )
 
-Base.rand( M::AbstractModel, n::Int ) = error( "rand not yet implemented for $(typeof(M))" )
+Distributions.rand!( M::AbstractModel, v::AbstractVector, n::Int = length(v) ) =
+    error( "rand not yet implemented for $(typeof(M))" )
+
+function Base.rand( M::AbstractModel, n::Int )
+    observations = zeros(n)
+    Distributions.rand!( M, observations )
+    return observations
+end
 
 mutable struct AdaptedModel{T <: AbstractModel}
     dates::Vector{Date}
@@ -52,6 +60,9 @@ function update( model::AdaptedModel, d::Date, y::Float64 )
     end
     update( model.models[model.index], y )
 end
+
+Distributions.rand!( model::AdaptedModel, v::AbstractVector{Float64}, n::Int = length(v) ) =
+    Distributions.rand!( model.models[model.index], v, n )
 
 Base.rand( model::AdaptedModel, n::Int ) = rand( model.models[model.index], n )
 
