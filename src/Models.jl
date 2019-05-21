c760922795a86c32a0416ec1bab919cb8d9523ce
Kevin Atteson <kevin@atteson.com>
2019-05-21 17:00:03 -0400

    first cut of refactor

--- src/Models.jl
+++ src/Models.jl
@@ -3,16 +3,20 @@ module Models
 using Dates
 using Distributions
 
-abstract type AbstractModel
+abstract type AbstractModel{T}
 end
 
 initialize( M::AbstractModel ) = error( "initialize not yet implemented for $(typeof(M))" )
 
-roll( M::AbstractModel ) = error( "roll not yet implemented for $(typeof(M))" )
+update( M::AbstractModel{T}, y::T ) where {T} = error( "update not yet implemented for $(typeof(M))" )
 
-update( M::AbstractModel, y::Float64 ) = error( "update not yet implemented for $(typeof(M))" )
+function update( M::AbstractModel{T}, a::AbstractVector{T} ) where {T}
+    for x in a
+        update( M, x )
+    end
+end
 
-Distributions.rand!( M::AbstractModel, v::AbstractVector, n::Int = length(v) ) =
+Distributions.rand!( M::AbstractModel{T}, v::AbstractVector{T}, n::Int = length(v) ) where {T} =
     error( "rand not yet implemented for $(typeof(M))" )
 
 function Base.rand( M::AbstractModel, n::Int )
@@ -21,13 +25,59 @@ function Base.rand( M::AbstractModel, n::Int )
     return observations
 end
 
-mutable struct AdaptedModel{T <: AbstractModel}
-    dates::Vector{Date}
-    models::Vector{T}
-    index::Int
+fit( M::AbstractModel ) = error( "fit not yet implemented for $(typeof(M))" )
+
+struct FittableModel{T, U <: AbstractModel{T},F <: Function} <: AbstractModel{T}
+    model::U
 end
 
-function AdaptedModel{T}( modeldir::String, d::Date ) where {T}
+FittableModel( model::U, ::F ) where {T,U <: AbstractModel{T},F} = FittableModel{T,U,F}( model )
+
+fit( model::FittableModel{T,U,F}; kwargs... ) where {T,U,F} = F.instance( model.model; kwargs... )
+
+update( model::FittableModel{T}, y::T ) where {T} = update( model.model, y )
+
+abstract type DatedModel{T} <: AbstractModel{Tuple{Date,T}}
+end
+
+update( model::DatedModel{T}, y::Tuple{Date,T} ) where {T} = error( "update not yet implemented for $(typeof(M))" )
+    
+date( M::DatedModel ) = error( "date not yet implemented for $(typeof(M))" )
+
+mutable struct LogReturnModel{T <: AbstractModel{Float64}} <: DatedModel{Float64}
+    model::T
+    lastdate::Date
+    lastprice::Float64
+end
+
+initialize( model::LogReturnModel ) = initialize( model.model )
+
+function update( model::LogReturnModel, y::Tuple{Date,Float64} )
+    update!( model.model, log(y[2]/model.lastprice) )
+    model.lastdate = y[1]
+    model.lastprice = y[2]
+end
+
+function Distributions.rand!( model::LogReturnModel, v::AbstractVector{Float64}, n::Int = length(v) ) where {T}
+    rand!( model.model, v, n=n )
+    lastprice = model.lastprice
+    for i = 1:n
+        v[i] = lastprice *= exp( v[i] )
+    end
+end
+
+fit( model::LogReturnModel ) = fit( model.model )
+
+date( model::LogReturnModel ) = model.lastdate
+
+mutable struct AdaptedModel{T,U <: AbstractModel{T}} <: DatedModel{T}
+    dates::Vector{Date}
+    models::Vector{U}
+    index::Int
+    lastprice::Float64
+end
+
+function AdaptedModel{T,U}( modeldir::String, d::Date ) where {T,U}
     fileregex = r"_([0-9]{8})$"
     dateformat = Dates.DateFormat( "yyyymmdd" )
     dates = Date[]
@@ -54,7 +104,7 @@ function AdaptedModel{T}( modeldir::String, d::Date ) where {T}
     return AdaptedModel( dates, models, index )
 end    
 
-function update( model::AdaptedModel, d::Date, y::Float64 )
+function update( model::AdaptedModel, d::Date, y::T ) where {T}
     while model.index < length(model.dates) && model.dates[model.index+1] < d
         model.index += 1
     end
@@ -63,7 +113,5 @@ end
 
 Distributions.rand!( model::AdaptedModel, v::AbstractVector{Float64}, n::Int = length(v) ) =
     Distributions.rand!( model.models[model.index], v, n )
-
-Base.rand( model::AdaptedModel, n::Int ) = rand( model.models[model.index], n )
 
 end # module
--- test/testmodels.jl
+++ test/testmodels.jl
@@ -1,7 +1,15 @@
 using Models
 using HMMs
 using Brobdingnag
+using Dates
+using Distributions
+using Random
 
-modeldir = joinpath( dirname(dirname(pathof(Models))), "data" )
+Random.seed!(1)
+hmm1 = rand( HMMs.HMM{2,Normal,Brob,Float64} )
+y = rand( hmm1, 1_000 );
 
-models = Models.AdaptedModel{HMMs.HMM{HMMs.GenTDist,Brob,Float64}}( modeldir )
+Random.seed!(2)
+hmm2 = Models.FittableModel( rand( HMMs.HMM{2,Normal,Brob,Float64} ), HMMs.em )
+Models.update( hmm2, y )
+Models.fit( hmm2, debug=2 )
