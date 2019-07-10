8037cd644cf2a0c9b9cdd6ec4ea272beabb55f12
Kevin Atteson <kevin@atteson.com>
2019-07-10 16:55:15 -0400

    putting aside

--- src/DistributionModels.jl
+++ src/DistributionModels.jl
@@ -1,6 +1,6 @@
 using Ipopt
 
-struct DistributionModel{D <: Distribution, U, T} <: AbstractModel{U,T}
+mutable struct DistributionModel{D <: Distribution, U, T} <: AbstractModel{U,T}
     d::D
 end
 
@@ -8,6 +8,10 @@ initialize( ::DistributionModel, args... ) = nothing
 
 inequalityconstraintmatrix( ::Type{M} ) where {U,T,M <: DistributionModel{Normal,U,T}} = [0.0 1.0]
 inequalityconstraintvector( ::Type{M} ) where {U,T,M <: DistributionModel{Normal,U,T}} = [0.0]
+
+getparameters( model::DistributionModel{D,T,U} ) where {D <: Distribution{Univariate}, T, U} = [params( model.d )...]
+setparameters!( model::DistributionModel{D,T,U}, parameters::AbstractVector{Float64} ) where {D, T, U} =
+    model.d = D( parameters... )
 
 function rand( ::Type{M};
                t = domaintype(M)[],
@@ -50,8 +54,20 @@ function dloglikelihood( model::DistributionModel{Normal,T,U},
                          u::AbstractVector{U},
                          dl::AbstractVector{Float64} ) where {D,T,U}
     (mu,sigma) = params(model.d)
+    n = length(u)
     dl[1] = sum(u .- mu)/sigma^2
-    dl[2] = sum((u .- mu).^2)/sigma^3 - 1/sigma
+    dl[2] = sum((u .- mu).^2)/sigma^3 - n/sigma
+end
+
+function d2loglikelihood( model::DistributionModel{Normal,T,U},
+                         t::AbstractVector{T},
+                         u::AbstractVector{U},
+                         d2l::AbstractMatrix{Float64} ) where {D,T,U}
+    (mu,sigma) = params(model.d)
+    n = length(u)
+    dl[1,1] = -count(u)/sigma^2
+    dl[1,2] = dl[2,1] = -2*sum(u .- m)/sigma^3
+    dl[2,2] = -3*sum((u .- mu).^2)/sigma^4 + n/sigma^2
 end
                                                                                       
 function fit( model::AbstractModel{T,U}, t::AbstractVector{T}, u::AbstractVector{U} ) where {T,U}
